cad37a81b721945843fb9375f90fbabbf8a7ad00
Julian Claus <kontakt@julian-claus.de>
2019-04-26 14:26:04 +0200

    test: add intro test

--- tests/unit/vue-command.spec.js
+++ tests/unit/vue-command.spec.js
@@ -15,15 +15,28 @@ describe('VueCommand.vue', () => {
     expect(wrapper.contains('.term-bar')).toBe(false)
   })
 
-  it('sets the title', () => {
-    const reversed = Array.from(VueCommand.props.title.default).reverse().join('')
+  it('sets the intro', () => {
+    const intro = Array.from(VueCommand.props.intro.default).reverse().join('')
     const wrapper = shallowMount(VueCommand, {
       propsData: {
         ...EMPTY_COMMANDS,
-        title: reversed
+        showIntro: true,
+        intro
       }
     })
 
-    expect(wrapper.find('.term-title').text()).toBe(reversed)
+    expect(wrapper.find('.term-cont:first-child').text()).toBe(intro)
+  })
+
+  it('sets the title', () => {
+    const title = Array.from(VueCommand.props.title.default).reverse().join('')
+    const wrapper = shallowMount(VueCommand, {
+      propsData: {
+        ...EMPTY_COMMANDS,
+        title
+      }
+    })
+
+    expect(wrapper.find('.term-title').text()).toBe(title)
   })
 })
