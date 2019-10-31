6e38934b2f6a6fb873d0efed8b99a76f8fb31b32
krmax44 <hi@krmax44.de>
2019-10-31 11:54:49 +0100

    refactor: drop lodash and ramda

--- src/mixins/autocomplete.js
+++ src/mixins/autocomplete.js
@@ -1,7 +1,3 @@
-import isEmpty from 'lodash/isEmpty'
-import isNull from 'lodash/isNull'
-import { and } from 'ramda'
-
 // @vue/component
 export default {
   data: () => ({
@@ -11,11 +7,11 @@ export default {
 
   methods: {
     // Calls the user given autocompletion resolver and sets the input accordingly
-    autocomplete ({ key }) {
-      if (and(!isEmpty(this.current), !isNull(this.autocompletionResolver))) {
+    autocomplete () {
+      if (this.current && typeof this.autocompletionResolver === 'function') {
         const autocomplete = this.autocompletionResolver(this.current, this.cursor)
 
-        this.bus.$emit('autocomplete', { command: `${autocomplete}`, uid: this._uid })
+        this.bus.$emit('autocomplete', { command: autocomplete, uid: this._uid })
       }
     },
 
