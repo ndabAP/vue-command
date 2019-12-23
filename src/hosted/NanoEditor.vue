4213fb019175aa3519bcd5121978823541512d37
krmax44 <hi@krmax44.de>
2019-12-23 19:44:24 +0100

    chore: simplify

--- src/hosted/NanoEditor.vue
+++ src/hosted/NanoEditor.vue
@@ -1,29 +1,21 @@
 <template>
-  <div v-if="!done">
-    <textarea ref="editor" @keydown.ctrl.88="finish">This is a text editor! Press Ctrl + X to leave.</textarea>
+  <div v-if="$running">
+    <textarea ref="editor" @keydown.ctrl.88="$done()">This is a text editor! Press Ctrl + X to leave.</textarea>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return { done: false }
-  },
   mounted () {
     this.$goFullscreen()
     this.$refs.editor.focus()
-  },
-  methods: {
-    finish () {
-      this.done = true
-      this.$done()
-    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-div, textarea {
+div,
+textarea {
   display: block;
   width: 100%;
   height: 100%;
