41d3ef53b8a10f37b3e2c49af228b930fec32e32
Julian Claus <kontakt@julian-claus.de>
2020-04-11 17:03:27 +0200

    refactor: simplify autocomplete

--- src/mixins/autocomplete.js
+++ src/mixins/autocomplete.js
@@ -8,7 +8,7 @@ export default {
 
   data: () => ({
     local: {
-      // Current cursor position at STDIN
+      // Current cursor position at Stdin
       cursor: 0
     }
   }),
@@ -23,9 +23,10 @@ export default {
     // Calls the user given autocompletion resolver and sets the input accordingly
     autocomplete () {
       if (this.local.current && typeof this.autocompletionResolver === 'function') {
+        // Retrieve users generated autocompletion result
         const autocomplete = this.autocompletionResolver(this.local.current, this.local.cursor)
 
-        this.bus.$emit('autocomplete', { stdin: autocomplete, uid: this._uid })
+        this.setCurrent(autocomplete)
       }
     },
 
