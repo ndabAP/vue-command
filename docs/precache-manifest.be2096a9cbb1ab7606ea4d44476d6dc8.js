73b759714ca3f6c0b5fc81681856cc38676ecaa4
Julian Claus <kontakt@julian-claus.de>
2022-12-15 16:22:30 +0100

    chore: infrastructure

---
+++ docs/precache-manifest.be2096a9cbb1ab7606ea4d44476d6dc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eae37904176fe10969f",
+    "url": "css/app.493bf05d.css"
+  },
+  {
+    "revision": "62051b6ac594d62923f77131e461249c",
+    "url": "index.html"
+  },
+  {
+    "revision": "2eae37904176fe10969f",
+    "url": "js/app.965936d8.js"
+  },
+  {
+    "revision": "f8326aff2684803baab7",
+    "url": "js/chunk-vendors.94f378d9.js"
+  },
+  {
+    "revision": "24534015ca4137a647e4fff735c9a671",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
